043d5151d8b0e62e27e27047ddeaeebe97d232d9
Al-Amin49 <alamin496832@gmail.com>
2022-10-15 01:31:03 +0600

    get all books

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const app=express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port=4000;
+//middleware
+app.use(express.json())
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5g2qurp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const booksCollection=client.db('book-spot').collection('books');
+
+        app.get('/book', async(req, res)=>{
+            const query={};
+            const cursor= booksCollection.find(query);
+            const books= await cursor.toArray();
+            res.send(books)
+        })
+    }
+    finally{
+
+    }
+}
+run()
+
+
+app.get('/', (req, res)=>{
+    res.send('Welcome to book spot');
+})
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}`)
+})
